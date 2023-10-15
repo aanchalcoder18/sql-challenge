e926aeca6ad7807d918be9bb43c2f54e92c61ae1
aanchalcoder18 <aasaxena14@gmail.com>
2023-10-15 13:29:35 -0700

    data models

---
+++ qdbdiagram.sql
@@ -0,0 +1,34 @@
+Dept_emp
+---
+emp_no PK int FK >- Employees.emp_no
+dept_no PK varchar FK >- departments.dept_no
+
+Employees
+---
+emp_no PK int
+emp_title_id varchar FK >- titles.title_id
+birth_date date
+first_name varchar
+last_name varchar
+sex varchar
+hire_date date
+
+Dept_mamanger
+---
+emp_no PK int FK >- Employees.emp_no
+dept_no PK varchar FK >- departments.dept_no
+
+titles
+---
+title_id PK varchar
+title varchar
+
+salaries 
+---
+emp_no PK int FK >- Employees.emp_no
+salary int
+
+departments
+---
+dept_no PK varchar
+dept_name varchar 
